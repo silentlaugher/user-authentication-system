837f5edaab110d5dc72a55c16e10a84543307f26
Christopher Edynak <christopher1edynak@gmail.com>
2020-11-02 11:40:44 -0500

    implemented bootstrap starter template and partials

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="utf-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-     <meta name="description" content="">
-     <meta name="author" content="Christopher Edynak">
-     <meta name="generator" content="Jekyll v4.1.1">
-     <title>Home Page</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php 
+    include_once 'includes/partials/headers.php';
+    include_once 'includes/partials/nav.php';
+?>
+
+<div class="container">
+
+  <div class="flag">
+    <h1>Edynak User Authentication System</h1>
+    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+  </div>
+
+</div><!-- /.container -->
+
+<?php 
+    include_once 'includes/partials/footers.php'
+?>
